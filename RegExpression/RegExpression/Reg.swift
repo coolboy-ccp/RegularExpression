6006bcb0f5c47d01405ecee17d61f87785ed1954
Brian.Chu <chuchengpeng0119@163.com>
2019-12-11 19:37:04 +0800

    邮箱

--- RegExpression/RegExpression/Reg.swift
+++ RegExpression/RegExpression/Reg.swift
@@ -9,9 +9,31 @@
 import UIKit
 
 
+enum MobileRegType {
+    case cm //移动
+    case cu //联通
+    case ct //电信
+    case all
+}
+
+extension MobileRegType {
+    var reg: String {
+        switch self {
+        case .cm:
+            return "^1(3[4-9]|4[7]|5[0-27-9]|7[8]|8[2-478]|9[8])\\d{8}$)|(^1705\\d{7}$"
+        case .cu:
+            return "^1(3[0-2]|4[5]|5[56]|66|7[56]|8[56])\\d{8}$)|(^1709\\d{7}$"
+        case .ct:
+            return "^1(33|53|77|8[019]|99)\\d{8}$)|(^1700\\d{7}$"
+        case .all:
+            return "^1(3[0-9]|4[57]|5[0-35-9]|6[6]|7[05-8]|8[0-9]|9[89])\\d{8}$"
+        }
+    }
+}
+
 enum Reg {
     case ID //中国大陆身份证
-    case mobile //手机号
+    case mobile(_ type: MobileRegType) //手机号
     case mail //邮箱
     case url //是否是一个链接
     case passprot //护照
@@ -28,17 +50,17 @@ extension Reg {
         case .ID:
             let area = "([1-6]{1}\\d{5})"
             let year = "([1-9]{1}\\d{3})"
-            let month = "((?:0[1-9]{1})|(?:1[1-2]{1}))"
-            let day = "((?:0[1-9]{1})|(?:[1-3]{1}[0-9]{1}))" //每月以31天考虑
+            let month = "(0[1-9]{1}|1[1-2]{1})"
+            let day = "(0[1-9]{1}|[1-2]{1}[0-9]{1}|3{1}[0-1]{1})" //每月以31天考虑
             return "\(area)\(year)\(month)\(day)(\\d{3})[xX0-9]{1}"
-        case .mobile:
-            return ""
+        case .mobile(let type):
+            return type.reg
         case .mail:
-            return ""
+            return "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
         case .passprot:
-            return ""
+            return "^1[45][0-9]{7}|([P|p|S|s]\\d{7})|([S|s|G|g]\\d{8})|([Gg|Tt|Ss|Ll|Qq|Dd|Aa|Ff]\\d{8})|([H|h|M|m]\\d{8，10})$"
         case .carID:
-            return ""
+            return "^[\\u4e00-\\u9fa5]{1}[a-zA-Z]{1}[a-zA-Z_0-9]{4}[a-zA-Z_0-9_\\u4e00-\\u9fa5]$"
         case .bankID:
             return ""
         case .num(let n):
@@ -98,6 +120,14 @@ extension NSString: RegSource {
 
 extension String {
     func isID() -> Bool {
+        
+        /*
+         1、将前面的身份证号码17位数分别乘以不同的系数。从第一位到第十七位的系数分别为：7－9－10－5－8－4－2－1－6－3－7－9－10－5－8－4－2。
+         2、将这17位数字和系数相乘的结果相加。
+         3、用加出来和除以11，看余数是多少？
+     4、余数只可能有0－1－2－3－4－5－6－7－8－9－10这11个数字。其分别对应的最后一位身份证的号码为1－0－X－9－8－7－6－5－4－3－2。(即余数0对应1，余数1对应0，余数2对应X...)
+         5、通过上面得知如果余数是3，就会在身份证的第18位数字上出现的是9。如果对应的数字是2，身份证的最后一位号码就是罗马数字x。
+         */
         if !Reg.ID.match(with: self) { return false }
         var ratios = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
         let rlts = [1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2]
